553c8586445a68ca8306e8d9c441ccb2a69f9dbe
Javier Azorín <javier.azorin@innocv.com>
2022-09-22 14:00:45 +0200

    Apple official SwiftUI tutorial part 1.

---
+++ Landmarks/CircleImage.swift
@@ -0,0 +1,22 @@
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .clipShape(Circle())
+            .overlay {
+                Circle()
+                    .stroke(.white, lineWidth: 4)
+            }
+            .shadow(radius: 7)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
